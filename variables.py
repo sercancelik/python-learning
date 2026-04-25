8816fc454862e59f6b7c3ce3298efda023ec3e9f
Sercan Çelik <sercancelik@users.noreply.github.com>
2026-04-25 20:04:11 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,26 @@
+# Variables don't need a type declaration, like in C or Pascal. Every variable in Python is an object.
+# A variable is created the moment you first assign a value to it.
+x = 5
+y = "Hello, World!"
+name = "Alice"
+age = 30
+height = 1.75
+is_student = True
+# You can also assign multiple values to multiple variables in one line:
+a, b, c = 1, 2, 3
+# And you can assign the same value to multiple variables in one line:
+d = e = f = 4
+# To output variables, you can use the print() function:
+print(x)
+print(y)
+print(name)
+print(age)
+print(height)
+print(is_student)
+print(a, b, c)
+print(d, e, f)
+# You can also use the type() function to find out the type of a variable:
+print(type(x))
+print(type(y))
+print(type(a))
+print(type(d))
